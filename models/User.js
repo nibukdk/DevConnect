8a18bc48da126296857b7fb1eff12dd5982b0008
Nibesh Khadka <nibeshkhadka@gmail.com>
2018-12-10 18:45:02 +0200

    User models for authentication

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+
+let UserSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String,
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+
+});
+
+
+const User = mongoose.model('Users', UserSchema);
+
+module.exports = User;
